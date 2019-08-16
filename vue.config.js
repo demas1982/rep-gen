8a7d8a66d635111cffa573ef28f4b87743602895
Дмитрий Коннов <konnov@outlook.com>
2019-08-16 10:22:06 +0900

    1

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     proxy: {
       '^/public': {
         target: 'http://10.28.11.66:4001/rep-gen/',
-        changeOrigin: true, // so CORS doesn't bite us. 
+        changeOrigin: true // so CORS doesn't bite us. 
       }
     }
   }
